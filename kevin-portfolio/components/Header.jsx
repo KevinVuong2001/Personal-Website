7bfed78852e92464005eb2a57188fddb06a0a0a0
Kevin Vuong <vuong.kevin.2001@gmail.com>
2025-01-01 20:52:44 -0800

    Modifying the Navigation Bar and Setting Up the Pages

---
+++ kevin-portfolio/components/Header.jsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+// Components
+import Nav from "./Nav";
+
+
+
+const Header = () => {
+    return (
+        <header className="py-8 xl:py-12 text-white">
+            <div className="container mx-auto flex justify-between items-center">
+                {/* logo */}
+                <Link href="/">
+                    <h1 className="text-4xl font-semibold">
+                        Kevin<span className="text-accent">.</span>
+                    </h1>
+                </Link>
+
+                {/* Desktop Navigation */}
+                <div className="hidden xl:flex items-center gap-8">
+                    <Nav />
+                    <Link href="/contact">
+                        <Button className="dark">Contact Me</Button>
+                    </Link>
+                </div>
+
+                {/* Mobile Navigation */}
+                <div className="xl:hidden">mobile nav</div>
+
+            </div>
+        </header>
+
+    );
+}
+
+export default Header;
