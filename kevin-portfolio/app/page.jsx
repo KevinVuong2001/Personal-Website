f4ef6697856eaefa5dacf318b1071247f179dee7
Kevin Vuong <vuong.kevin.2001@gmail.com>
2025-01-18 23:51:17 -0800

    Initial Start to the Homepage including introduction, download button, and socials

--- kevin-portfolio/app/page.jsx
+++ kevin-portfolio/app/page.jsx
@@ -1,7 +1,46 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
 
-export default function Home() {
+// Components
+import Socials from "@/components/Socials";
+
+
+const Home = () => {
   return (
-    <main>homepage</main>
-  );
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1">
+              Hello, I'm <br /> <span>Kevin Vuong</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/70">
+            A passionate and driven software developer with experience creating dynamic, user-friendly applications. 
+            Proficient in a variety of programming languages and technologies, 
+            I am committed to continuously enhancing the skills I’ve gained through education and internships while expanding my knowledge and expertise in emerging technologies.
+            </p>
+            { /* btn and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                <span>Download CV/Resume</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Socials 
+                  containerStyles="flex gap-6" 
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent
+                  hover:text-primary hover:transition-all duration-500"/>
+              </div>
+            </div>
+          </div>
+          {/* photo */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  )
 }
+
+export default Home;
---
+++ kevin-portfolio/components/Socials.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+// Define your social links
+const socials = [
+    { icon: <FaGithub />, path: "https://github.com/KevinVuong2001" },
+    { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/kevin-vuong-b47664235/" }
+  ];
+  
+
+// Socials component to render the list of social icons
+const Socials = ({ containerStyles, iconStyles }) => {
+    return <div className={containerStyles}>
+    {socials.map((item, index) => {
+        return <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+        </Link>
+    })}
+</div>;
+};
+
+export default Socials;
