866593f853ce2425951f039116ebdf4a54eac791
KevinVuong2001 <vuong.kevin.2001@gmail.com>
2025-02-18 15:49:10 -0800

    Added sliders and details about personal projects

--- kevin-portfolio/app/projects/page.jsx
+++ kevin-portfolio/app/projects/page.jsx
@@ -22,28 +22,29 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 const personal_projects = [
     {
         num: "01",
-        category: "WonderTix",
+        category: "Full-Stack",
+        name: "WonderTix",
         title: "project 1",
         description:
         `For my senior capstone, I collaborated with eight colleagues to develop WonderTix, an open-source software project sponsored by Portland Playhouse and initiated at Portland State University in 2021. 
         WonderTix is a comprehensive sales platform designed to manage task assignments, generate financial reports, and handle ticket sales. 
-        In my role, I gained hands-on experience in full-stack development. 
         At the start of the project, the financial reports relied on hardcoded data, which limited flexibility for future updates. 
         To address this, I developed a Restful API endpoint to deliver critical financial information dynamically. 
-        I also created a Sales Overview Report leveraging the new API, which served as a blueprint for integrating additional reports through a scalable backend approach.`,
-        stack: [ {name: "#React"}, {name: "#TypeScript"}, {name: "#RESTfulAPIs"}, {name: "Node.js"}, {name: "PostgreSQL"} ],
+        I also created a Sales Overview Report leveraging the new API, implemented in React and TypeScript which served as a blueprint for integrating additional reports through a scalable backend approach.`,
+        stack: [ {name: "#React"}, {name: "#TypeScript"}, {name: "#RESTfulAPIs"}, {name: "#Node.js"}, {name: "#PostgreSQL"} ],
         image: '/projects/Portland_Playhouse.jpeg',
         github: "https://github.com/WonderTix/WonderTix",
     },
     {
         num: "02",
-        category: "NBA Fantasy League",
+        category: "Full-Stack/Cloud",
+        name: "NBA Fantasy League",
         title: "project 2",
         description:
         `Developed a dynamic NBA Fantasy League web application utilizing the Yahoo Fantasy Sports API and OAuth 2.0 authentication to provide users with real-time access to their fantasy league data. 
         The application enables users to track player stats, view standings, search for free agents, and manage their rosters seamlessly. 
         By integrating Yahoo's API, the platform retrieves up-to-date fantasy data, allowing users to make informed decisions for their teams.`,
-        stack: [ {name: "#Python"}, {name: "#Flask"}, {name: "#OAuth2"}, {name: "#RESTfulAPIs"}, {name: "#HTML"}, {name: "#Bootstrap"}, {name: "#JavaScript"}],
+        stack: [ {name: "#Python"}, {name: "#Flask"}, {name: "#OAuth2"}, {name: "#GoogleCloud"}, {name: "#Firestore"}, {name: "#RESTfulAPIs"}, {name: "#HTML"}, {name: "#Bootstrap"}, {name: "#JavaScript"}],
         image: '/projects/NBA_Fantasy_Home.png',
         github: "https://github.com/KevinVuong2001/Yahoo-NBA-Fantasy-League-Project",
     },
@@ -128,58 +129,179 @@ const internship_projects = [
     },
 ]
 
-const Projects = () => {      
-    const [activeTab, setActiveTab] = useState("personal_project");
-    const render_personal_project = (projects) => {
-        return(
-            <div>Personal Portfolio</div>
-        )
-    }
-    const render_internship_project = (projects) => {
-        return(
-            <div>Internship Portfolio</div>
-        )
-    }
-    return (
-        <motion.section
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        className="min-h-[70vh] flex flex-col justify-center py-12 xl:px-0"
-    >
-        <div className="container mx-auto flex flex-col items-start">
-            <Tabs
-                defaultValue="personal_project"
-                className="flex flex-col gap-[60px]"
-            >
-                {/* Centering the TabsList */}
-                <TabsList className="flex flex-row gap-6 justify-center mx-auto xl:mx-0 gap-6">
-                    <TabsTrigger
-                        value="personal_project"
-                        onClick={() => setActiveTab("personal_project")}
-                    >
-                        Personal Projects
-                    </TabsTrigger>
-                    <TabsTrigger
-                        value="internship_project"
-                        onClick={() => setActiveTab("internship_project")}
-                    >
-                        Internship Projects
-                    </TabsTrigger>
-                </TabsList>
+const keywords = ["open-source", 
+                "API", 
+                "Restful API", 
+                "scalable backend approach", 
+                "financial reports", "dynamically", 
+                "Sales Overview Report", 
+                "WonderTix", 
+                "blueprint", 
+                "sponsored by Portland Playhouse",
+                "React",
+                "TypeScript",
+                "dynamic",
+                "OAuth 2.0",
+                "real-time access",
+                "web application",
+                "up-to-date"
+            ];
 
-                {/* Tabs Content below the TabsTrigger */}
-                <div>
-                    <TabsContent value="personal_project" className="items-left">
-                        {activeTab === "personal_project" && render_personal_project(personal_projects)}
-                    </TabsContent>
-                    <TabsContent value="internship_project">
-                        {activeTab === "internship_project" && render_internship_project(internship_projects)}
-                    </TabsContent>
+const highlightKeywords = (text, keywords) => {
+    // Create a regex pattern dynamically from the keywords list
+    const pattern = new RegExp(`\\b(${keywords.join("|")})\\b`, "gi");
+
+    return text.split(pattern).map((part, index) => 
+        keywords.includes(part) 
+            ? <span key={index} className="font-extrabold text-accent">{part}</span> 
+            : part
+    );
+};
+
+const ProjectDescription = ({ description }) => {
+    return <p className="text-white/70 text-sm md:text-base leading-relaxed">{highlightKeywords(description, keywords)}</p>;
+};
+
+const Projects = () => {      
+    const [activeProjectType, setActiveProjectType] = useState("personal");
+    const [personalProject, setPersonalProject] = useState(personal_projects[0]);
+    const [internshipProject, setInternshipProject] = useState(internship_projects[0]);
+    
+    const handlePersonalSlideChange = (swiper) => {
+        // Get Current Index
+        const currentIndex = swiper.activeIndex;
+        //Update project state based on current index
+        setPersonalProject(personal_projects[currentIndex])
+    }
+
+    // Function to render Personal Projects
+    const renderPersonalProject = (project) => (
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+            <div className="w-full xl:w-[70%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+                {/* outline num */}
+                <div className="text-8xl leading-none font-extrabold">
+                    {project.num}
                 </div>
-            </Tabs>
+                {/* project name and category*/}
+                <div className="group space-y-3 md:space-y-4">
+                    <h2 className="text-3xl md:text-[42px] font-bold leading-none group-hover:text-accent transition-all duration-500 capitalize">
+                        {project.name}
+                    </h2>
+                    <h2 className="text-2xl md:text-[42px] font-bold leading-none group-hover:text-accent transition-all duration-500 capitalize">
+                        {project.category} Project
+                    </h2>
+
+                    {/* Description */}
+                    <ProjectDescription 
+                            description={activeProjectType === "personal" ? personalProject.description : internshipProject.description} 
+                    />
+                    {/* Stack */}
+                    <ul className="flex flex-wrap gap-4">
+                        {project.stack.map((item, index) => {
+                            return (
+                                <li key={index} className="text-xl text-accent">
+                                    {item.name}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    {/* Border */}
+                    <div className="border-b border-white/20"></div>
+                    {/* GitHub Buttons */}
+                    <Link href={project.github}>
+                        <TooltipProvider delayDuration={100}>
+                            <Tooltip>
+                                <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                                    <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                                </TooltipTrigger>
+                                <TooltipContent className="bg-white text-black font-semibold">
+                                    <p>GitHub Repository</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        </TooltipProvider>
+                    </Link>
+                </div>
+            </div>
+            <div className="w-full xl:w-[52%]">
+                <Swiper
+                    spaceBetween={30}
+                    slidesPerView={1}
+                    className="xl:h-[520px] mb-12"
+                    onSlideChange={handlePersonalSlideChange}
+                >
+                    {personal_projects.map((p, index) => {
+                        return <SwiperSlide key={index} className="w-full">
+                            <div className="h-[460px] w-full relative group flex justify-center items-center bg-pink-50/60">
+                                {/* Overlay */}
+                                <div className="absolute top-0 left-0 w-full h-full">
+                                    <img src={project.image} alt="Project Screenshot" className="w-full h-full object-cover"/>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    })}
+                </Swiper>
+            </div>
         </div>
-    </motion.section>
-    )   
-}
+    );
+
+    // Function to render Internship Projects
+    const renderInternshipProject = (project) => (
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+            <div className="w-full xl:w-[70%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+                <h2 className="text-4xl font-bold">{project.category}</h2>
+                <div className="space-y-4">
+                    <p className="text-lg text-gray-300"><span className="font-semibold">Problem:</span> {project.problem}</p>
+                    <p className="text-lg text-gray-300"><span className="font-semibold">Contribution:</span> {project.contribution}</p>
+                    <p className="text-lg text-gray-300"><span className="font-semibold">Impact:</span> {project.impact}</p>
+                </div>
+            </div>
+            <div className="w-full xl:w-[50%] xl:pl-[60px]">
+                <img src={project.image} alt="Project Screenshot" className="rounded-lg shadow-lg"/>
+            </div>
+        </div>
+    );
+
+    return(
+        <div className="container mx-auto flex flex-col items-start py-6">
+            {/* Buttons to Toggle Project Type */}
+            <div className="flex gap-4 mb-3">
+                <button 
+                    className={`px-6 py-2 text-lg font-semibold rounded-md transition-all ${
+                        activeProjectType === "personal" ? "bg-accent text-black" : "bg-gray-700 text-white/70"
+                    }`}
+                    onClick={() => setActiveProjectType("personal")}
+                >
+                    Personal Projects
+                </button>
+                <button 
+                    className={`px-6 py-2 text-lg font-semibold rounded-md transition-all ${
+                        activeProjectType === "internship" ? "bg-accent text-black" : "bg-gray-700 text-white/70"
+                    }`}
+                    onClick={() => setActiveProjectType("internship")}
+                >
+                    Internship Projects
+                </button>
+            </div>
+
+            {/* Motion Section for Animation */}
+            <motion.section
+                initial={{ opacity: 0 }}
+                animate={{ 
+                    opacity: 1, 
+                    transition: { delay: 2.4, duration: 0.4, ease: "easeIn"}
+                }}
+                className="min-h-[70vh] flex flex-col justify-center py-12 xl:px-0"
+            >
+                <div className="container mx-auto">
+                    {/* Conditional Rendering of Project Content */}
+                    {activeProjectType === "personal" 
+                        ? renderPersonalProject(personalProject) 
+                        : renderInternshipProject(internshipProject)
+                    }
+                </div>
+            </motion.section>
+        </div>
+    );
+};
 
 export default Projects
